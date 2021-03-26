08514bb38b0012c41761cf46857316ac782dc2b6
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-26 18:52:59 -0400

    range

--- com/dictionary/nesting.py
+++ com/dictionary/nesting.py
@@ -1,2 +1,7 @@
+aliens = []
+for alien in range(30):
+    new_aliens = {'color': 'green', 'point': 4, 'speed:': 'slow'}
+    aliens.append(new_aliens)
 
-print('nesting loop')
+for alien in aliens[:5]:
+    print(alien)
