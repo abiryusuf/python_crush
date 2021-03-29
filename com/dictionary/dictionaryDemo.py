36125864bf9f0dad21a99a83c78dd74fb2a78706
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-28 23:44:27 -0400

    count word

--- com/dictionary/dictionaryDemo.py
+++ com/dictionary/dictionaryDemo.py
@@ -9,6 +9,21 @@ info['email'] = 'abir.com'
 print(info)
 
 file_counts = {"jpg": 10, "text": 14, "csv": 2, "py": 23}
+print(file_counts.items())
+
+# for key in file_counts.keys():
+#     print(key)
 
 for keys, values in file_counts.items():
     print('THere are {} files with the {}'.format(keys, values))
+
+sentence = 'I am abir'
+def count_letter(text):
+    result = {}
+    for letter in text:
+        if letter not in result:
+            result[letter] = 0
+        result[letter] += 1
+    return result
+print(count_letter(sentence))
+
