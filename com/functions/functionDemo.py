9f234a1dbbccc9f34985db0362967379da31a4d1
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-29 23:14:29 -0400

    update

--- com/functions/functionDemo.py
+++ com/functions/functionDemo.py
@@ -17,3 +17,37 @@ def add(x, y):
     print("Total: " + str(x + y))
 
 add(3, 5)
+# positional argument
+# multiple function calls and follow the order argument
+def info(first_name, last_name, age):
+    print('\nMy first name is: ' + first_name.title())
+    print("My last name is: " + last_name.title())
+    print("My full name is {} {} and age is {}".format(first_name.title(), last_name.title(), age))
+
+info("abir", 'yusuf', 32)
+info("mim", 'monira', 4)
+
+# return value
+def name(first_name, last_name):
+    fullName = first_name + ' ' + last_name
+    return fullName.title()
+x = name('abir', 'yusuf')
+print(x)
+
+def get_formatted(first, last, middle=''):
+    if middle:
+        full_name = first + ' ' + middle + ' ' + last
+    else:
+        full_name = first + ' ' + last
+    return full_name.title()
+
+y = get_formatted('abir', 'yusuf')
+print(y)
+x = get_formatted('a', 'v', 'c')
+print(x)
+
+
+
+
+
+
