9194d4798ba380847e0966c72b293b51ebc50121
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-29 20:15:02 -0400

    basic function

--- com/functions/functionDemo.py
+++ com/functions/functionDemo.py
@@ -3,3 +3,17 @@ def greet_user(userName):
     print("Hello " + userName.title())
 
 greet_user('Abir')
+
+""""Try it yourself"""
+def display_message():
+    print("What are you learning about this chapter")
+display_message()
+
+def favorite_book(book):
+    print("One of my favorite books is " + book.title())
+favorite_book("How to influence people")
+
+def add(x, y):
+    print("Total: " + str(x + y))
+
+add(3, 5)
