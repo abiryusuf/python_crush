277635021983e923b286359bec21420e6cce8d79
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-31 22:01:10 -0400

    function is complicated

--- com/functions/passing_list.py
+++ com/functions/passing_list.py
@@ -8,3 +8,22 @@ def greet_user(names):
 username = ["abir", 'yusuf', 'mim']
 greet_user(username)
 
+# Modifying a list in a function
+designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+# for n in designs:
+#     completed_models.append(designs)
+#     print(completed_models)
+while designs:
+    current_designs = designs.pop()
+    print("Printing model " + current_designs)
+# Simulate creating a 3d print from the design
+    completed_models.append(current_designs)
+
+# Display all completed models
+print("\nThe following models have been printed")
+for completed_model in completed_models:
+    print("\t" + completed_model)
+
+# for design in designs:
+#     print("Printing model " + design)
