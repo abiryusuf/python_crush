fe856c8985a1aba86dc8bde36f1aa3e7a53746b3
abiryusuf <yusuf_ctg@yahoo.com>
2021-04-02 20:04:31 -0400

    alias

--- com/functions/stroing/making_pizza.py
+++ com/functions/stroing/making_pizza.py
@@ -1,4 +1,6 @@
 import pizza
-
+# if function is conflict or long name, I can use a short, unique alias
+from pizza import make_pizza as x
+x(45, "abr")
 pizza.make_pizza(16, 'pepperoni')
 pizza.make_pizza(12, 'av', 'cd')
