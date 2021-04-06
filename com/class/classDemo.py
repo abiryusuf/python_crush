805a2491eeac4ed182c725189766f807238b662a
abiryusuf <yusuf_ctg@yahoo.com>
2021-04-05 21:14:31 -0400

    class

--- com/class/classDemo.py
+++ com/class/classDemo.py
@@ -7,15 +7,15 @@ ab = MyClass()
 print(ab.x)
 
 class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+  def __init__(x, name, age):
+    x.name = name
+    x.age = age
 
-  def myfunc(self):
-    print("Hello my name is " + self.name)
+  def myfunc(y):
+    print("Hello my name is " + y.name)
 
 p1 = Person("Abir", 30)
-
+p1.age = 23
 print("My name is {} and age is {}".format(p1.name, p1.age))
 p1.myfunc()
 
