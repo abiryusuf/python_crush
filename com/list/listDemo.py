a6c83bac38692e82176efb1baf19285a5cdcee64
abiryusuf <yusuf_ctg@yahoo.com>
2021-04-12 22:49:22 -0400

    list

--- com/list/listDemo.py
+++ com/list/listDemo.py
@@ -3,25 +3,32 @@
 # list is a collection which is ordered and changeable. And allow the duplicates values
 
 names = ['abir', 'yusuf', 'mim']
-print(len(names))
+# print(len(names))
 
 # add new name end of the list
-names.append("madiha")
-print(names)
+# names.append("madiha")
+# print(names)
 
 # insert the name based on the index number
-names.insert(0, "munni")
-print(names)
+# names.insert(0, "munni")
+# print(names)
 
 # removing the names from list
-del names[0]
+# del names[0]
+# print(names)
+
+# sort, the method follow the alphabetically order
+names.sort()
+print(names)
+# or
+names.sort(reverse=True)
 print(names)
 
 # range
 
 # index
-y = names[0].title()
-print(y)
+# y = names[0].title()
+# print(y)
 
 for name in names:
     print('\nMy name is ' + name.title())
