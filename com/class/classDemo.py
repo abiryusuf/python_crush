f0821221192ac3de09740ab03c62ba35a34c5119
abiryusuf <yusuf_ctg@yahoo.com>
2021-04-12 21:58:55 -0400

    list

--- com/class/classDemo.py
+++ com/class/classDemo.py
@@ -1,23 +1,25 @@
-
-
 class MyClass:
     x = 8
+
 
 ab = MyClass()
 print(ab.x)
 
-class Person:
-  def __init__(x, name, age):
-    x.name = name
-    x.age = age
 
-  def myfunc(y):
-    print("Hello my name is " + y.name)
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def myfunc(self):
+        print("Hello my name is " + self.name)
+
 
 p1 = Person("Abir", 30)
 p1.age = 23
 print("My name is {} and age is {}".format(p1.name, p1.age))
 p1.myfunc()
+
 
 class Person2:
     def __init__(self, fname, lname):
@@ -27,10 +29,14 @@ class Person2:
     def fullName(self):
         print(self.firstName, self.lastName)
 
+
 y = Person2('yusuf', 'abir')
 y.fullName()
 
+
 class Students(Person2):
     pass
+
+
 t = Students("Mim", 'Madiha')
 t.fullName();
---
+++ com/list/listDemo.py
@@ -0,0 +1,6 @@
+# list is the collection of data or items in single variable
+# list follow the [] brackets
+# list is a collection which is ordered and changeable. And allow the duplicates values
+
+names = ['abir', 'yusuf', 'mim']
+print(len(names))
