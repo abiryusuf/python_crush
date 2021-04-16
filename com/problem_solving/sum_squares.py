0a7718815ad1e07cc1b599cf15538d1c3590a1ce
abiryusuf <yusuf_ctg@yahoo.com>
2021-04-16 17:14:15 -0400

    update

--- com/problem_solving/sum_squares.py
+++ com/problem_solving/sum_squares.py
@@ -9,9 +9,17 @@ values = [5, 6, 78, 9, 23]
 
 def sumNum(num):
     sum = 0
-    ave = 0
+
     for value in num:
         sum += value
-        ave += 1
+
     return sum
+
 print('Total ' + str(sumNum(values)))
+
+animals = ["Lion", "Zebra", "Dolphin", "Monkey"]
+char = 0
+
+for animal in animals:
+    char += len(animal)
+    print("Total character: {}, Average length {}".format(char, char/len(animal)))
