eacb011e9e39b0a75ad17cf60d121c66ae636a10
abiryusuf <yusuf_ctg@yahoo.com>
2021-04-16 01:48:23 -0400

    break and continue

--- com/loop/forLoop.py
+++ com/loop/forLoop.py
@@ -10,9 +10,38 @@ colors = ['red', 'black', 'white', 'green']
 # for names in name:
 #     print(names)
 
-
+# Break
 # With break statement we can stop loop before it has looped through the all item
-for color in colors:
-    if color == 'white':
-        break
-    print("Break " + str(color))
+# for color in colors:
+#     if color == 'white':
+#         break
+#     print("Break " + color)
+
+# continue
+# we can stop the the current iteration of the loop and continue the next
+# for con in colors:
+#     if con == 'white':
+#         continue
+#     print('\nContinue ' + con)
+
+# range
+# It is possible to specify the starting value by adding a parameter
+# for num in range(1, 10+2):
+#     print(num)
+
+# def rang(n):
+#     for number in range(1, n + 1):
+#         return number
+# print((rang(10)))
+
+
+# for number in range(1, 10+1, 3):
+#     print(number)
+
+# else
+for x in range(1, 10):
+    if x == 4:
+        continue
+    print(x)
+else:
+    print('DONE')
