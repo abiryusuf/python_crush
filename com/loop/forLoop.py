f1ba089abf77372977544f3dc27c0117f2752dc4
abiryusuf <yusuf_ctg@yahoo.com>
2021-04-16 00:29:33 -0400

    loop with string

---
+++ com/loop/forLoop.py
@@ -0,0 +1,13 @@
+# the loop is execute the set of statements
+
+colors = ['red', 'black', 'white', 'green']
+
+# for color in colors:
+#     print("\nMy favorite color " + color.title())
+
+# loop through the a string
+# name = 'abir yusuf'
+# for names in name:
+#     print(names)
+
+
