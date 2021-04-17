607512f51d5b5c7c45a1704df8466f2b27315731
abiryusuf <yusuf_ctg@yahoo.com>
2021-04-17 16:33:56 -0400

    update

--- com/list/listDemo.py
+++ com/list/listDemo.py
@@ -5,30 +5,56 @@
 names = ['abir', 'yusuf', 'mim']
 # print(len(names))
 
+names.reverse()
+print("Reverse the list: ")
+print(names)
+
 # add new name end of the list
 # names.append("madiha")
 # print(names)
 
 # insert the name based on the index number
-names.insert(0, "munni")
-print(names)
+# names.insert(0, "munni")
+# print(names)
 
 # removing the names from list
 # del names[0]
 # print(names)
 
-# sort, the method follow the alphabetically order
-names.sort()
-print(names)
-# or reverse the list
-names.sort(reverse=True)
-print(names)
+# # sort, the method follow the alphabetically order
+# names.sort()
+# print(names)
 
+# or reverse the list
+# names.sort(reverse=True)
+# print(names)
+
+
+# Sorted
+# print("Here is the orginal list: ")
+# print(names)
+# print("\nHere is the sorted list:")
+# print(sorted(names))
+#
+# print("\nHere is the original list")
+# print(names)
 # range
 
 # index
 # y = names[0].title()
 # print(y)
 
-for name in names:
-    print('\nMy name is ' + name.title())
+# for name in names:
+#     print('\nMy name is ' + name.title())
+
+# TRY IT
+# places = ['New York', 'Bangladesh', 'Switzerland', 'Paris', 'Denmark']
+# print("Original list: ")
+# print(places)
+# print("\nHere is the sorted list: ")
+# print(sorted(places))
+# print("\nHere is the original list:")
+# print(places)
+
+places.reverse()
+print(places)
