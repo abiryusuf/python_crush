c10a103f76d17208c0d016794365d9b63b589f73
abiryusuf <yusuf_ctg@yahoo.com>
2021-04-18 01:16:09 -0400

    enumerate

--- com/list/list_comprehensions.py
+++ com/list/list_comprehensions.py
@@ -11,9 +11,25 @@ print(multiple)
 multiple = [x*2 for x in range(1, 10)]
 print(multiple)
 
+
 # conditions
 add = [y + 2 for y in range(1, 10 + 1) if y % 2 == 0]
 print(add)
+
+# how to find length or word from list
+languages = ["Java", "Perl", "Python", "C++"]
+def length_list(num):
+    lengths = [len(x) for x in num]
+    return "word length " + str(lengths)
+print(length_list(languages))
+
+for language in languages:
+    print(len(language))
+
+for index, language in enumerate(languages):
+    print("{} - {}".format(index + 1, len(language)))
+
+
 
 
 
---
+++ com/list/list_enumerate.py
@@ -0,0 +1,12 @@
+# what if you want to access the elements in a list,
+# along with the index of the element in question?
+# You can do this using the enumerate () function.
+# The enumerate () function takes a list as a parameter
+# and returns a tuple for each element in the list.
+# The first value of the tuple is the index,
+# and the second value is the element itself.
+
+winner = ['Abir', 'Yusuf', 'Mim', 'Madiha']
+
+for index, person in enumerate(winner):
+    print("{}. - {}".format(index + 1, person))
