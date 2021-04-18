38a8c171f26d94b256fcc322bd151a04d3260dea
abiryusuf <yusuf_ctg@yahoo.com>
2021-04-18 16:55:16 -0400

    get words

---
+++ com/problem_solving/get_word.py
@@ -0,0 +1,18 @@
+sentence = "I am abir"
+
+for letter in sentence:
+    print(letter)
+
+words = sentence.split()
+print(len(words))
+
+for word in words:
+    print(word)
+
+def get_word(sentence, n):
+    if n > 0:
+        words = sentence.split()
+        if n <= len(words):
+            return (words[n-1])
+    return("")
+print(get_word("This is a lesson about lists", 4))
