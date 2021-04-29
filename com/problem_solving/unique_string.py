8287f430798e149f509ce302ca3dc3486ec51967
abiryusuf <yusuf_ctg@yahoo.com>
2021-04-29 04:37:05 -0400

    updated

--- com/problem_solving/unique_string.py
+++ com/problem_solving/unique_string.py
@@ -1,17 +1,46 @@
-def unique_string(string):
-    string.replace(" ", "")
-    char = set()
+# def unique_string(string):
+#     string.replace(" ", "")
+#     char = set()
+#
+#     for letter in string:
+#         if letter in char:
+#             return False
+#         else:
+#             char.add(letter)
+#     return True
+#
+# def unique(string):
+#     str = string.replace(" ", "")
+#
+#     return len(set(str)) == len(str)
+#
+# print(unique('abirr'))
+# print(unique_string("a b c d"))
 
-    for letter in string:
-        if letter in char:
-            return False
+# def duplicate_value(num):
+#
+#     seen = set()
+#     uniq = ""
+#
+#     for value in num:
+#         if value not in seen:
+#             uniq.append(value)
+#             seen.add(value)
+#     return uniq
+#
+# print(duplicate_value([1, 2, 2, 4, 3]))
+
+# without set
+def value(num):
+    seen = {}
+    dup = []
+
+    for x in num:
+        if x not in seen:
+            seen[x] = 1
         else:
-            char.add(letter)
-    return True
-
-def unique(string):
-    str = string.replace(" ", "")
-    return len(set(str)) == len(str)
-
-print(unique('a b c d d'))
-print(unique_string(" a b c d d"))
+            if seen[x] == 1:
+                dup.append(x)
+            seen[x] += 1
+    return dup
+print(value([1, 2, 2, 3, 3, 4]))
