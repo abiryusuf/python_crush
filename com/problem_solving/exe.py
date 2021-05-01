3c9719e8542e51f7d807c87481e13f104b244467
abiryusuf <yusuf_ctg@yahoo.com>
2021-05-01 02:06:44 -0400

    updated

--- com/problem_solving/exe.py
+++ com/problem_solving/exe.py
@@ -1,22 +1,63 @@
-sen = "I am abir yusuf"
+# def duplicate(num):
+#     dup = []
+#     seen = set()
+#
+#     for value in num:
+#         if value not in seen:
+#             dup.append(value)
+#             seen.add(value)
+#     return dup
+#
+#
+# print(duplicate([1, 1, 2, 2, 2, 5, 6, 6]))
+#
+# # list comprehensions
+# def list_duplicate(n):
+#     seen = set()
+#     uniq = [x for x in n if x not in seen and not seen.add(x)]
+#     return uniq
+# print(list_duplicate([1, 2, 3, 3, 4, 4]))
+#
+# def sum(num):
+#     res = 0
+#
+#     for x in range(1, num + 1):
+#         res += x
+#     return res
+#
+#
+# print(sum(5))
+#
+#
+# def factorial(n):
+#     count = 1
+#
+#     for x in range(1, n + 1):
+#         count = count * x
+#     return count
+#
+#
+# x = factorial(5)
+#
+# print("Factorial " + str(x))
 
-def count_letter(text):
-    res = {}
+#
+def total(n):
+    sum = 0
+    for value in n:
+        sum += n[value]
+    return round(sum, 2)
+groceries = {"bananas": 1.56, "apples": 2.50, "oranges": 0.99, "bread": 4.59, "coffee": 6.99, "milk": 3.39, "eggs": 2.98, "cheese": 5.44}
+number = [1, 2, 4, 5]
+x = total(groceries)
+print("Total Price " + str(x))
 
-    for letter in text:
-        if letter in res:
-            res[letter.lower()] += 1
-        else:
-            res[letter.lower()] = 1
-    return res
+number = [1, 2, 4, 5]
 
-print(count_letter(sen))
+def sumNum(n):
+    sum = 0
 
-def reverse(str):
-
-    res = ""
-    for x in str:
-        if x not in res:
-            res = x + res
-    return res
-print(reverse("abir yusuf"))
+    for x in n:
+        sum += x
+    return sum
+print("Total {}".format(sumNum(number)))
--- com/problem_solving/unique_string.py
+++ com/problem_solving/unique_string.py
@@ -17,30 +17,30 @@
 # print(unique('abirr'))
 # print(unique_string("a b c d"))
 
-# def duplicate_value(num):
-#
-#     seen = set()
-#     uniq = ""
-#
-#     for value in num:
-#         if value not in seen:
-#             uniq.append(value)
-#             seen.add(value)
-#     return uniq
-#
-# print(duplicate_value([1, 2, 2, 4, 3]))
+def duplicate_value(num):
+
+    seen = set()
+    uniq = []
+
+    for value in num:
+        if value not in seen:
+            uniq.append(value)
+            seen.add(value)
+    return uniq
+
+print(duplicate_value([1, 2, 2, 4, 3, 6, 6, 6]))
 
 # without set
-def value(num):
-    seen = {}
-    dup = []
-
-    for x in num:
-        if x not in seen:
-            seen[x] = 1
-        else:
-            if seen[x] == 1:
-                dup.append(x)
-            seen[x] += 1
-    return dup
-print(value([1, 2, 2, 3, 3, 4]))
+# def value(num):
+#     seen = {}
+#     dup = []
+#
+#     for x in num:
+#         if x not in seen:
+#             seen[x] = 1
+#         else:
+#             if seen[x] == 1:
+#                 dup.append(x)
+#             seen[x] += 1
+#     return dup
+# print(value([1, 2, 2, 3, 3, 4]))
