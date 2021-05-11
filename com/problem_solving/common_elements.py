6b1064ee6d15f40d384515cc2439f64b32d2edbe
abiryusuf <yusuf_ctg@yahoo.com>
2021-05-11 01:57:59 -0400

    updated

--- com/problem_solving/common_elements.py
+++ com/problem_solving/common_elements.py
@@ -11,10 +11,11 @@ def common_element(a, b):
             result.append(a[num1]) and result.append(b[num2])
             num1 += 1
             num2 += 1
-        elif a[num1] < b[num2]:
-            num1 += 1
-        else:
-            num2 += 1
+        # elif a[num1] < b[num2]:
+        #     num1 += 1
+        # else:
+        #     num2 += 1
+
     return result
 
 print((common_element([1, 3, 4, 5], [1, 2, 3, 4])))
