ca159ba82f48ff05d5e80c389a1ea064cd85e23a
abiryusuf <yusuf_ctg@yahoo.com>
2021-06-30 22:13:31 -0400

    algo

--- com/exe.py
+++ com/exe.py
@@ -52,51 +52,68 @@
 #     return unique
 # print(duplicateNum(duplicate))
 
-def countVowels(str):
-    res = 0
-    str = str.replace(" ", "").lower()
+# def countVowels(str):
+#     res = 0
+#     str = str.replace(" ", "").lower()
+#
+#     for char in str:
+#         if char in "aeiou":
+#             res = res + 1
+#     return res
+#
+# print(countVowels("abir yUsuf"))
+#
+#
+# groceries = {"bananas": 1.56, "apples": 2.50, "oranges": 0.99,
+#  "bread": 4.59, "coffee": 6.99, "milk": 3.39, "eggs": 2.98,
+#  "cheese": 5.44}
+#
+# number = [2, 5, 8, 9]
+# def sum(num):
+#     total = 0
+#     for i in num:
+#         total += num[i]
+#     return round(total, 2)
+# print(sum(groceries))
+#
+# def factorial(num):
+#     res = 1
+#     for i in range(1, num + 1):
+#         res = res * i
+#     return res
+# print(factorial(5))
+#
+# # for c in range(1, 6, 2):
+# #     print(c)
+#
+# def total(num):
+#     sum = 0
+#
+#     for i in range(1, num + 1):
+#         sum += i
+#     return sum
+# print(total(5))
+#
+# num = [2, 5, 6]
+#
+#
+# def maxNum(num):
+#     max = 0
+#     n = len(num)
+#     for x in range(n):
+#         current = num[x]
+#         if current > max:
+#             max = current
+#     return max
+# print(maxNum(num))
 
-    for char in str:
-        if char in "aeiou":
-            res = res + 1
-    return res
+def anagram(str1, str2):
+    string1 = str1.replace(" ", "").lower()
+    string2 = str2.replace(" ", "").lower()
+    word1 = sorted(string1)
+    word2 = sorted(string2)
+    if word1 == word2:
+        return True
+    return False
 
-print(countVowels("abir yUsuf"))
-
-
-groceries = {"bananas": 1.56, "apples": 2.50, "oranges": 0.99,
- "bread": 4.59, "coffee": 6.99, "milk": 3.39, "eggs": 2.98,
- "cheese": 5.44}
-
-number = [2, 5, 8, 9]
-def sum(num):
-    total = 0
-    for i in num:
-        total += num[i]
-    return round(total, 2)
-print(sum(groceries))
-
-def factorial(num):
-    res = 1
-    for i in range(1, num + 1):
-        res = res * i
-    return res
-print(factorial(5))
-
-# for c in range(1, 6, 2):
-#     print(c)
-
-def total(num):
-    sum = 0
-
-    for i in range(1, num + 1):
-        sum += i
-    return sum
-print(total(5))
-
-num = [2, 5, 6]
-n = len(num)
-for i in range(n):
-    x = num[i]
-    print(x)
-print(type(x))
+print(anagram("I am abr", "I am abir"))
