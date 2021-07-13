f3d43397a50106c6970a22278a98cd9e535564ab
abiryusuf <yusuf_ctg@yahoo.com>
2021-07-12 20:34:42 -0400

    update

--- com/problem_solving/convertDict.py
+++ com/problem_solving/convertDict.py
@@ -65,14 +65,20 @@ newList = [12, 35, 9, 56, 24]
 print(swapList(newList))
 
 def swap(listNew):
-
-    res = []
     size = len(listNew)
+
     first = listNew[0]
-    last = listNew[-1]
+    listNew[0] = listNew[size-1]
+    listNew[size - 1] = first
 
-    res = last + first
+    return listNew
 
-    return res
 print(swap(newList))
+
+
+for num in range(100, 200):
+    # if all(num % i != 0 for i in range(2, num)):
+    #     print(num)
+    if num % 2 == 0:
+        print("Prime Number: " + str(num))
 
