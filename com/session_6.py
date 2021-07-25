b899713c35e71e48f58ce3b454371f6a4cdcd045
abiryusuf <yusuf_ctg@yahoo.com>
2021-07-25 15:15:01 -0400

    update

--- com/session_6.py
+++ com/session_6.py
@@ -10,3 +10,20 @@ for i, value in enumerate(x):
     res[i] = value
 print(str(res))
 
+
+" Python Program to find largest element in an array "
+num = [3, 6, 8, -3, -4]
+def largestNum(n):
+    size = len(n)
+
+    if len(n) == 0:
+        return False
+    max = n[0]
+    min = n[0]
+
+    for i in range(1, size):
+        current = n[i]
+        if current < min:
+            min = current
+    return min
+print(largestNum(num))
